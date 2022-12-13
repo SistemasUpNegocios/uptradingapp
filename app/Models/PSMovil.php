10dc9607af093246d6a9b1442b05035b57ff8c69
JavierSalazarUp <javiersalazar@uptradingexperts.com>
2022-12-13 10:49:54 -0600

    PSMovil models

---
+++ app/Models/PSMovil.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PSMovil extends Model
+{
+    use HasFactory;
+
+    protected $table = "psmovil";
+    public $timestamps = false;
+}
